7b0c4e3fac74d65e711f9bbd80e953196378cc98
leafN <1e4fnetwork@gmail.com>
2021-01-19 11:13:48 +0900

    problem number fix+add 43164

--- programmers_43164.swift
+++ programmers_43164.swift
@@ -1,43 +1,34 @@
 import Foundation
 
-var arr: [Int] = [Int]()
-
-func parent(_ a: Int) -> Int {
-    if arr[a] == a { return a }
-    arr[a] = parent(arr[a])
-    return arr[a]
-}
-
-func union(_ a: Int, _ b: Int) {
-    let a = parent(a)
-    let b = parent(b)
-    if a < b { arr[b] = a }
-    else { arr[a] = b }
-}
-
-func find(_ a: Int, _ b: Int) -> Bool {
-    let a = parent(a)
-    let b = parent(b)
-    if a == b { return true }
-    else { return false }
-}
-
-func solution(_ n:Int, _ computers:[[Int]]) -> Int {
-    arr = [Int](0..<n)
-    for (i_0, v_0) in computers.enumerated() {
-        for (i_1, v_1) in v_0.enumerated() {
-            if i_0 == i_1 { continue }
-            else {
-                if v_1 == 1 { union(i_0, i_1) }
+var result = [String]()
+func dfs(_ tickets: [[String]], _ travels: [String], _ count: Int) {
+    if travels.count == count + 1 {
+        if result.count == 0 {
+            result = travels
+        } else {
+            for i in 0..<travels.count {
+                if travels[i] < result[i] {
+                    result = travels
+                    return
+                } else if travels[i] > result[i] { return }
+            }
+        }
+    } else {
+        for (index, ticket) in tickets.enumerated() {
+            if ticket[0] == travels.last! {
+                var tickets = tickets
+                tickets.remove(at: index)
+                var travels = travels
+                travels.append(ticket[1])
+                dfs(tickets, travels, count)
             }
         }
     }
-    
-    return Array(Set(arr.map { parent($0) })).count
 }
 
-solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])
-solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]])
-solution(6, [[1, 0, 1, 1, 0, 0], [0, 1, 0, 0, 1, 1], [1, 0, 1, 1, 1, 1], [1, 0, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1]])
-solution(3, [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-solution(4, [[1, 0, 0, 1], [0, 1, 1, 0], [0, 1, 1, 0], [1, 1, 0, 1]])
+func solution(_ tickets:[[String]]) -> [String] {
+    
+    result = [String]()
+    dfs(tickets, ["ICN"], tickets.count)
+    return result
+}
