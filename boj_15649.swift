96bc4e4fd8954560b63f2d193a1c6faf901a981f
leafN <1e4fnetwork@gmail.com>
2021-02-01 15:34:52 +0900

    add backtracking problems

---
+++ boj_15649.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func foo(_ res: [Int], _ arr: [Int], _ limit: Int) {
+    guard res.count < limit else {
+        var res = res
+        let first = res.removeFirst()
+        print(res.map({ String($0) }).reduce(String(first), {$0 + " " + $1 }))
+        return
+    }
+    for i in 0..<arr.count {
+        var arr = arr
+        let first = arr[i]
+        _ = arr.remove(at: i)
+        var res = res
+        res.append(first)
+        foo(res, arr, limit)
+    }
+}
+
+if let inp = readLine()?.split(separator: " ").map({ Int(String($0))! }) {
+    let (n, m) = (inp[0], inp[1])
+    
+    foo([], [Int](1...n), m)
+}
